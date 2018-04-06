ab6e2c5da51cb33491539c5ed1b605319e339335
Katarzyna Jedrzejczak <katarzyna.jedrzejczak@op.pl>
2018-04-06 22:19:51 +0200

    Update files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import App from './App';
 import reducer from './reducer';
