16bcb3f526e9c390cdacba0418ea62bf0650a9a1
Michał Gacek <gacek.michael@outlook.com>
2018-04-12 15:52:37 +0200

    Improvements

--- src/Comment.js
+++ src/Comment.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-const Comment = ({text, votes, id, thumbUpComment, thumbDownComment}) => 
+const Comment = ({text, votes, id, thumbUpComment, thumbDownComment, edit, remove}) => 
     <li>
         {text}
         <span> votes: {votes}</span>
         <div className="buttons"> 
         	<button onClick={() => thumbUpComment(id)}>Thumb up</button>
         	<button onClick={() => thumbDownComment(id)}>Thumb down</button>
+            <button onClick={() => edit(id)}>Edit</button>
+            <button onClick={() => remove(id)}>Delete</button>
         </div>
     </li>;
 
